86be0410c6c615e13199876b0ac200cbb6d3f049
fakhri ramdani <muhammadlukman09@gmail.com>
2019-10-10 21:32:55 +0700

    Project KeTigapuluh

---
+++ src/pboul/pkg10115229/latihan30/cakep/PBOUL10115229Latihan30Cakep.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan30.cakep;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Lukman
+ */
+public class PBOUL10115229Latihan30Cakep {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String jawab;
+        int pernyataan1;
+        String 
+        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Kamu ngerjain sendiri latihan 17 sampe latihan30? ");
+        System.out.print("Jawab(Yoi/Enggak) : ");
+        
+        jawab = keyboard.nextLine();
+                
+        if (pernyataan1 >=yoi){
+            
+            
+        
+                
+            
+        }
+    }
+    
+}
